e3fc334239956020f296adb50262c89b701f7865
Illidan <illidanrulez@gmail.com>
2023-03-10 08:13:43 +0100

    Temporarily disable silicon wheels.

--- pymkpsxiso.cpp
+++ pymkpsxiso.cpp
@@ -1,8 +1,4 @@
 #define VERSION "2.02"
-// Fix compilation on apple silicon
-#ifndef stat64
-    #define stat64 stat
-#endif
 // This is kind of dirty, but the main.cpp file for both programs includes a lot of code
 // They both use a Main function so we need to change the name
 #define Main dumpsxiso_main
