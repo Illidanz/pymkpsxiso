5354a14584c190034b29d26d330f1e878454248c
Illidan <illidanrulez@gmail.com>
2025-01-07 15:11:01 +0100

    Build python 3.13 wheels.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ for dir in INCLUDES:
 
 def main():
     setup(name="pymkpsxiso",
-          version="0.1.6",
+          version="0.1.7",
           author="Illidan",
           description="Python interface for mkpsxiso.",
           long_description=long_description,
